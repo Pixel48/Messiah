97e25611b3a4eb04b8d367b29c3c7b502bb844e3
Pixel48 <piksel48@gmail.com>
2021-04-03 20:05:40 +0200

    Re-enable logging, found a name-swap bug
    This reverts commit 2ba3e913e12b221d942bc4b753d33ecfbcd1d648.

--- src/Messiah.py
+++ src/Messiah.py
@@ -1,7 +1,7 @@
 import logging
 LOG_FORMAT = '[%(levelname)s] %(message)s'
 logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT)
-logging.disable(logging.CRITICAL)
+logging.disable(logging.NOTSET)
 logging.debug("Imported Logging")
 
 from tkinter import *;                logging.debug("Imported tkinter.*")
